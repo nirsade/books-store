498f60279a3b0824dcba9ff29480edac3beb7767
nir sade <nirsade@nirs-mbp.mynet>
2017-02-12 03:45:58 +0200

    Fix for Heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(functoin (req, res, next) {
+app.use(function (req, res, next) {
     if (req.headers['x-forwarded-proto'] === 'http') {
         next();
     } else {
