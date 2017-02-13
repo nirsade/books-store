a72c7713e2f98969b6130a02989026bf1d6dbd0a
nir sade <nirsade@nirs-mbp.mynet>
2017-02-13 15:57:52 +0200

    Change response error condition

--- app/api/FlickrApi.jsx
+++ app/api/FlickrApi.jsx
@@ -8,7 +8,7 @@ module.exports = {
         // Http Request for 30 photos in Json format
         var requestUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=8955e069ffdba2273dc398e6c796ad29&tags=${search}&per_page=30&format=json&nojsoncallback=1`;        
         return axios.get(requestUrl).then(function (res) {
-            if (res.data.cod && res.data.message) {
+            if (res.data.message) {
                 throw new Error(res.date.message);
             } else {
                 // Return the photos array from the server
